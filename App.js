806413cfddbcf65c9431fc851efc347d139160ad
Abhishek Mandal <amandal.eduu@gmail.com>
2022-08-25 17:25:52 +0530

    Test

--- App.js
+++ App.js
@@ -80,7 +80,7 @@ const App: () => Node = () => {
           <Section title="Debug">
             <DebugInstructions />
           </Section>
-          <Section title="Learn More">hy Abhishek:</Section>
+          <Section title="Learn More">Hello Abhishek:</Section>
           <LearnMoreLinks />
         </View>
       </ScrollView>
